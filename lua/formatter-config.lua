3ed620a637d95c2a5db02a17a65b7c4ac5b58735
AndersFelde <anderskfelde@hotmail.com>
2021-06-09 10:33:40 +0200

    gitignore

--- lua/formatter-config.lua
+++ lua/formatter-config.lua
@@ -20,6 +20,16 @@ require("formatter").setup(
                     }
                 end
             },
+            typescript = {
+                -- prettier
+                function()
+                    return {
+                        exe = "prettier",
+                        args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+                        stdin = true
+                    }
+                end
+            },
             markdown = {
                 -- prettier
                 function()
