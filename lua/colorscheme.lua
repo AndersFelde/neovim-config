2fc45d1b9d36702a79beb2669220bec52db812e9
AndersFelde <anderskfelde@hotmail.com>
2021-12-20 12:36:27 +0100

    fjerna italic

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,5 @@
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_functions = false
 vim.cmd("colorscheme tokyonight")
 -- require("material").set()
